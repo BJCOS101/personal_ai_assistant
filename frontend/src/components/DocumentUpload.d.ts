75740993c2c677a06d95a6a787885a12a6b45249
BJCOS101 <s4095502@student.rmit.edu.au>
2025-12-04 20:20:19 +1100

    after testing, frontend is now working ... now appears on webpage

--- frontend/src/components/DocumentUpload.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface DocumentUploadProps {
-    onUploadComplete: () => void;
-}
-export declare const DocumentUpload: React.FC<DocumentUploadProps>;
-export {};
--- frontend/src/components/Message.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Message as MessageType } from '../types';
-interface MessageProps {
-    message: MessageType;
-}
-export declare const Message: React.FC<MessageProps>;
-export {};
